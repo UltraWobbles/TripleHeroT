36c9af6338cca1de443257ceeef7a3b365e8db34
UltraWobbles <legendaryzebra08@gmail.com>
2026-08-24 10:57:57 +1200

    Adding the enemy movement

--- hero-game/Scripts/scene_1.gd
+++ hero-game/Scripts/scene_1.gd
@@ -2,5 +2,10 @@ extends Node3D
 
 @onready var Player = $Player
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
+	const move_speed := 4.0
+	%PathFollow3D.progress += move_speed * delta
+	
 	get_tree().call_group("enemies", "update_target_location", Player.global_transform.origin)
+	
+	
